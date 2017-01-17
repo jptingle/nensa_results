4b1e59335eba8f0af09ab796c07b756ca9f19b68
Jeffrey Tingle <jptingle@strivelabs.com>
2017-01-16 21:47:40 -0500

    updated ranking view to include explicit selects on average calculations, added rankings template page

--- views/rankings_view.sql
+++ views/rankings_view.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE VIEW MEMBER_SEASON_TOP_RESULTS AS
-SELECT s.season, m.nensa_num, m.first as 'First Name', m.last as 'Last Name', 
-        m.sex as 'Sex', e.Division, s.age_group,
-        COUNT(e.USSA_Result) as '# Races',
-        MIN(e.USSA_Result) as 'Best Race Results',
+SELECT s.season as 'Season', m.nensa_num as 'NENSA', m.first as 'First_Name', m.last as 'Last_Name', 
+        m.sex as 'Sex', e.Division as 'Division', s.age_group as 'Age_Group',
+        COUNT(e.USSA_Result) as '#_Races',
+        MIN(e.USSA_Result) as 'Best_Race_Result',
         (SELECT USSA_Result
             FROM RACE_RESULTS
             WHERE member_season_id=s.id AND USSA_Result <> 0
@@ -31,7 +31,7 @@ SELECT s.season, m.nensa_num, m.first as 'First Name', m.last as 'Last Name',
             WHERE member_season_id=s.id and USSA_Result <> 0
             GROUP BY USSA_Result HAVING COUNT(*) > 0
             ORDER BY USSA_Result ASC
-            LIMIT 1,1))/2) AS 'Avg Top 2',
+            LIMIT 1,1))/2) as "Avg_2_Best",
         (((SELECT USSA_Result
             FROM RACE_RESULTS
             WHERE member_season_id=s.id AND USSA_Result <> 0
@@ -42,12 +42,12 @@ SELECT s.season, m.nensa_num, m.first as 'First Name', m.last as 'Last Name',
             WHERE member_season_id=s.id and USSA_Result <> 0
             GROUP BY USSA_Result HAVING COUNT(*) > 0
             ORDER BY USSA_Result ASC
-            LIMIT 1,1)+(SELECT USSA_Result
+            LIMIT 1,1))+(SELECT USSA_Result
             FROM RACE_RESULTS
             WHERE member_season_id=s.id and USSA_Result <> 0
             GROUP BY USSA_Result HAVING COUNT(*) > 0
             ORDER BY USSA_Result ASC
-            LIMIT 2,1))/2) AS 'Avg Top 3'
+            LIMIT 2,1)/3) as "Avg_3_Best"
 FROM 
     MEMBER_SEASON s
         INNER JOIN 
@@ -55,12 +55,11 @@ FROM
         INNER JOIN 
     MEMBER_SKIER m ON m.member_id = s.member_id
 WHERE e.USSA_RESULT <> 0
-GROUP BY s.id
-ORDER BY Best_USSA_Result;
+GROUP BY s.id;
 
 CREATE OR REPLACE VIEW MEMBER_SEASON_RANKINGS AS
-SELECT `season`, `nensa_num`, `First Name`, `Sex`, `Last Name`, `Division`, `age_group`, `# Races`, 
-`Best Race Results`, `Best_USSA_Result`, `2ndBest_USSA_Result`, `3rdBest_USSA_Result`,
+SELECT `Season`, `NENSA`, `First Name`, `Sex`, `Last Name`, `Division`, `Age Group`, `# Races`, 
+`Best Race Result`, `Best USSA Result`, `2ndBest USSA Result`, `3rdBest USSA Result`,
 (Best_USSA_Result+2ndBest_USSA_Result)/2 AS 'Avg Top 2', 
 (Best_USSA_Result+2ndBest_USSA_Result+3rdBest_USSA_Result)/3 AS 'Avg Top 3'
 FROM MEMBER_SEASON_TOP_RESULTS;
--- wordpress_code/wp-content/themes/llorix-one-lite/template-rankings.php
+++ wordpress_code/wp-content/themes/llorix-one-lite/template-rankings.php
@@ -16,27 +16,7 @@
 <!-- /END HOME / HEADER  -->
 
 <script language=JavaScript>
-function reload(form)
-{
-	var val=form.season.options[form.season.options.selectedIndex].value;
-	var url = [location.protocol, '//', location.host, location.pathname].join('');
-	self.location=url + '?season=' + val;
-}
-function reload3(form)
-{
-	var val=form.season.options[form.season.options.selectedIndex].value;
-	var val1=form.event_id.options[form.event_id.options.selectedIndex].value;
-	var url = [location.protocol, '//', location.host, location.pathname].join('');
-	self.location=url + '?season=' + val + '&event_id=' + val1;
-}
-function reloadall(form)
-{
-	var val=form.season.options[form.season.options.selectedIndex].value;
-	var val1=form.event_id.options[form.event_id.options.selectedIndex].value;
-	var val2=form.event_name.options[form.event_name.options.selectedIndex].value;
-	var url = [location.protocol, '//', location.host, location.pathname].join('');
-	self.location=url + '?season=' + val + '&event_id=' + val1 + '&event_name=' + val2;
-}
+
 </script>
 
 <div class="content-wrap">
@@ -63,65 +43,62 @@ function reloadall(form)
 
 			<?php
 
-				if ( !isset( $results_db ) ) {
-					$results_db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME_RESULTS, DB_USER, DB_PASSWORD);
-				}
-	
-				if ( !isset( $results_db ) ) { echo "DB not available"; }
-				$quer2="SELECT DISTINCT season FROM race_event order by season"; 
 				if (array_key_exists("season",$_GET)) {
 					$season = $_GET['season'];
 				} 
-				if(isset($season) and strlen($season) > 0) {
-					$quer="SELECT DISTINCT event_name, event_id FROM race_event WHERE parent_event_id IS NULL AND season=$season order by event_name"; 
-				}
 
 				if (array_key_exists("event_id",$_GET)) {
 					$event_id = $_GET['event_id'];
 				} 
-				if(isset($event_id) and strlen($event_id) > 0) {
-					$quer3="SELECT DISTINCT event_name FROM race_event where parent_event_id=$event_id order by event_name"; 
-				} 
+
+				$season_array = array(2017);
+        $gender_array = array('F','M');
+        $age_group_array = array('U16','U18','U20','U23','SR');
 
 				echo "<form method=post name=f1>";
-				echo "<select name='season' onchange=\"reload(this.form)\"><option value=''>Select one</option>";
-				foreach ($results_db->query($quer2) as $noticia2) {
-					if($noticia2['season']==@$season) {
-						echo "<option selected value='$noticia2[season]'>$noticia2[season]</option>"."<BR>";}
+				echo "<select name='season' ><option value=''>Select season</option>";
+				foreach ($season_array as $noticia2) {
+					if(isset($_POST['season']) && ($_POST['season'] == $noticia2)) {
+						$season = $_POST['season'];
+						echo "<option selected value='$season'>$season</option>"."<BR>";}
 					else { 
-						echo "<option value='$noticia2[season]'>$noticia2[season]</option>";
+						echo "<option value='$noticia2'>$noticia2</option>";
 					}
 				}
 				echo "</select>";
 				echo "</br></br>";
-				echo "<select style='width:400px' name='event_id' onchange=\"reload3(this.form)\"><option value=''>Select one</option>";
-				foreach ($results_db->query($quer) as $noticia) {
-					if($noticia['event_id']==@$event_id) {
-						echo  "<option selected value='$noticia[event_id]'>$noticia[event_name]</option>";
+				echo "<select name='event_id'><option value=''>Select gender</option>";
+				foreach ($gender_array as $noticia) {
+					if(isset($_POST['event_id']) && ($_POST['event_id'] == $noticia)) {
+						$event_id = $_POST['event_id'];
+						echo  "<option selected value='$event_id'>$event_id</option>";
 					} else {
-						echo  "<option value='$noticia[event_id]'>$noticia[event_name]</option>";
+						echo  "<option value='$noticia'>$noticia</option>";
 					}
 				}
 				echo "</select>";
 				echo "</br></br>";
-				echo "<select style='width:400px' name='event_name'><option value=''>Select one</option>";
-				foreach ($results_db->query($quer3) as $noticia) {
-                                        if(isset($_POST['event_name'])){
-                                                $event_name = $_POST['event_name'];
-						echo  "<option selected value='$event_name'>$event_name</option>";
-                                        } else {	
-                                                echo  "<option selected value='$noticia[event_name]'>$noticia[event_name]</option>";
-                                        }
+				echo "<select name='age_group'><option value=''>Select age group</option>";
+				foreach ($age_group_array as $noticia) {
+          if(isset($_POST['age_group']) && $_POST['age_group'] == $noticia){
+            $age_group = $_POST['age_group'];
+						echo  "<option selected value='$age_group'>$age_group</option>";
+	        } else {	
+	          echo  "<option value='$noticia'>$noticia</option>";
+	        }
 				}
 				echo "</select>";
 				echo "</br></br>";
 				echo "<input type=submit value=Submit>";
 				echo "</form>";
 				$selected_val = "";
-				if(isset($_POST['event_name'])){
-					$selected_val = $_POST['event_name'];  // Storing Selected Value In Variable
-					$shortcode = '[wpdatatable id=8 table_view=regular]';
+				if(isset($_POST['age_group'])){
+					$season = $_POST['season'];
+					$gender = $_POST['event_id'];
+					$age_group = $_POST['age_group'];  // Storing Selected Value In Variable
+					$shortcode = '[wpdatatable id=9 VAR2="'.$event_id.'" VAR3="'.$age_group.'" table_view=regular]';
           echo "</br><hr>";
+          #echo $_POST['age_group']." ".$_POST['event_id']." ".$_POST['season'];
 			    echo do_shortcode($shortcode); 
 				}
 			?>
